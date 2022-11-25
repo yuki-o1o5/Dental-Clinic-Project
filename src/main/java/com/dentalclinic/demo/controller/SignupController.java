f829f309cd0bbd5bf50e5d5a44fb62224fb33ee6
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2022-11-25 17:30:36 +0900

    Create user in users tbl

--- src/main/java/com/dentalclinic/demo/controller/SignupController.java
+++ src/main/java/com/dentalclinic/demo/controller/SignupController.java
@@ -5,38 +5,37 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.dentalclinic.demo.dao.iSignupRepository;
 import com.dentalclinic.demo.entity.Signup;
+import com.dentalclinic.demo.service.UserService;
 
 @Controller
- @RequestMapping("/signup") 
 public class SignupController {
- 
+
   @Autowired
   iSignupRepository signupRepository;
-  
-  // @RequestMapping(value="/new", method = RequestMethod.GET)
-  @GetMapping //feedback?
+
+  @Autowired
+  UserService userService;
+
+  @GetMapping("/signup")
   public String displayProjectForm(Model model) {
     model.addAttribute("signup", new Signup());
     return "signup/signup";
   }
 
-  @PostMapping("/save")
+  @PostMapping("/signup/save")
   public String signup(Signup signup, Model model) {
-    System.out.println(signup.getFirstName());
-    System.out.println(signup.getLastName());
-    System.out.println(signup.getBirthDate());
-    System.out.println(signup.getEmail());
-    System.out.println(signup.getPhoneNumber());
-    System.out.println(signup.getPassword());
-    System.out.println("###################");
-
-    signupRepository.save(signup);
-    return "redirect:/home";  
-    // return "redirect:/signup/new";   
+    userService.create(
+      signup.getEmail(),
+      signup.getPassword(),
+      signup.getFirstName(),
+      signup.getLastName(),
+      signup.getBirthDate(),
+      signup.getPhoneNumber()
+    );
+    return "redirect:/home";
   }
 
 
